c90dcbadb6a4a9d90feba0d9dfc96fce6a435d63
Izzat Fahmi Alasadi <izzat.alasadi@gmail.com>
2024-04-23 13:21:30 +0200

    Initial commit of my Flask application

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from search_engine import create_app, socketio
 from search_engine.config import ProductionConfig,DevelopmentConfig
 
 # Create app instance with specific config
-app = create_app(DevelopmentConfig)
+app = create_app(ProductionConfig)
 
 if __name__ == '__main__':
     # Run the app with SocketIO support and debugging enabled
--- search_engine/config.py
+++ search_engine/config.py
@@ -1,44 +1,46 @@
+
 import os
 
 class Config(object):
-    # General Config
+    # General Configuration
     SECRET_KEY = os.environ.get('SECRET_KEY', 'CPr4ftfxzHDhzxRwJxv3aNEv5ItikxsCNxhUNmaDag8=')
     FLASK_APP = 'run.py'
     FLASK_ENV = 'development'
-
-    # Database
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mydatabase.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Session
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        uri = os.getenv('DATABASE_URL')
+        if uri and uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+        # Ensure SSL mode is used where necessary
+        if uri and 'heroku' in uri:
+            return uri + "?sslmode=require"
+        return uri
+
+    # Session Configuration
     SESSION_TYPE = 'filesystem'
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_FILE_DIR = os.environ.get('FLASK_SESSION_DIR') or './flask_session'
+    SESSION_FILE_DIR = os.environ.get('FLASK_SESSION_DIR', './flask_session')
 
-    # CSRF
+    # CSRF Protection
     WTF_CSRF_ENABLED = True
-    
-    # File
-    UPLOAD_FOLDER = '/Data/uploads/'
+
+    # File Uploads
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', '/Data/uploads/')
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_ECHO = True  # If you want to see SQLAlchemy queries for debugging
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use in-memory SQLite database for tests
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use in-memory SQLite for tests
 
 class ProductionConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
-    SESSION_COOKIE_SECURE = True
-
-    @property
-    def SQLALCHEMY_DATABASE_URI(self):
-        uri = os.getenv("DATABASE_URL")
-        if uri and uri.startswith("postgres://"):
-            uri = uri.replace("postgres://", "postgresql://", 1)
-        return uri + "?sslmode=require" if uri else None
-
+    SESSION_COOKIE_SECURE = True  # Use secure cookies in production
+    SECRET_KEY = os.environ.get('SECRET_KEY')  # Ensure secret key is set in production
