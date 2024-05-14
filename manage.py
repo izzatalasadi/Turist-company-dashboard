372a238451dc82980844c3029c19bf27eaf92ceb
Izzat Fahmi Alasadi <izzat.alasadi@gmail.com>
2024-05-14 17:54:32 +0200

    intial

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ from search_engine import create_app, socketio
 from search_engine.config import ProductionConfig,DevelopmentConfig
 
 # Create app instance with specific config
-app = create_app(ProductionConfig)
+app = create_app(DevelopmentConfig)
 
 if __name__ == '__main__':
     # Run the app with SocketIO support and debugging enabled
-    socketio.run(app)
+    socketio.run(app, debug=True)
