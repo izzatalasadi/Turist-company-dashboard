ba627be49babd71e19e7f7620dfb3f5fe68fd952
Izzat Fahmi Alasadi <izzat.alasadi@gmail.com>
2024-05-20 05:33:04 +0200

    Fix WebSocket handling with eventlet and gunicorn

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 # manage.py
 from search_engine import create_app, socketio
 from search_engine.config import ProductionConfig,DevelopmentConfig
+import os
 
 # Create app instance with specific config
 app = create_app(ProductionConfig)
@@ -8,4 +9,4 @@ app = create_app(ProductionConfig)
 if __name__ == '__main__':
     # Run the app with SocketIO support and debugging enabled
     #app.run()
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
