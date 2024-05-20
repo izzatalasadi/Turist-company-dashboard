38dbbe34b8c5bded868fea696c5b52368a2b1ac2
Izzat Fahmi Alasadi <izzat.alasadi@gmail.com>
2024-05-20 04:18:21 +0200

    Add config

--- search_engine/config.py
+++ search_engine/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY','key')
-    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_APP = 'run.py'
     FLASK_ENV = 'development'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -27,10 +27,9 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 class ProductionConfig(Config):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    FLASK_APP = os.environ.get('FLASK_APP')
-    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_APP = os.environ.get('FLASK_APP','manage.py')
+    FLASK_ENV = os.environ.get('FLASK_ENV','development')
     DEBUG = False
     TESTING = False
     SESSION_COOKIE_SECURE = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db').replace("postgres://", "postgresql://", 1) if os.environ.get('DATABASE_URL') else 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
