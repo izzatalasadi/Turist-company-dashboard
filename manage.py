683e1f0a6fda6ac46785a78e1d9d94a37eecb571
Izzat Fahmi Alasadi <izzat.alasadi@gmail.com>
2024-05-23 17:46:00 +0200

    Docker container

--- manage.py
+++ manage.py
@@ -9,4 +9,4 @@ app = create_app(ProductionConfig)
 if __name__ == '__main__':
     # Run the app with SocketIO support and debugging enabled
     #app.run()
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
